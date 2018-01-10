aeb9ed28ae27c1c3e09f81c95ebf0f1d37becd09
Angela Jiang <to.ajiang@gmail.com>
2018-01-10 14:15:03 -0700

    Add cached urban tracker commands

---
+++ scripts/experiment/urban_cmds.sh
@@ -0,0 +1,32 @@
+
+# sherbrooke 
+
+labels="/datasets/BigLearning/ahjiang/bb/urban-tracker/labels/car-label.txt"
+model_prefix="data/models/exp/yolov2-sherbrooke-car"
+plot_prefix="data/plots/urban-tracker/sherbrooke/car/pr"
+output="data/images/urban-tracker/sherbrooke/car"
+input="/datasets/BigLearning/ahjiang/bb/npz/urban-tracker-sherbrooke-car-test.npz"
+
+# stmarc
+
+labels="/datasets/BigLearning/ahjiang/bb/urban-tracker/labels/car-label.txt"
+model_prefix="data/models/exp/yolov2-stmarc-car"
+plot_prefix="data/plots/urban-tracker/stmarc/car/pr"
+output="data/images/urban-tracker/stmarc/car"
+input="/datasets/BigLearning/ahjiang/bb/npz/urban-tracker-stmarc-car-test.npz"
+
+# hybrid
+
+labels="/datasets/BigLearning/ahjiang/bb/urban-tracker/labels/car-label.txt"
+model_prefix="data/models/exp/yolov2-rouen-sherbrooke-stmarc-car"
+output="data/images/urban-tracker/hybrid/car/"
+plot_prefix="data/plots/urban-tracker/hybrid/car/pr"
+input="/datasets/BigLearning/ahjiang/bb/npz/urban-tracker-rouen-sherbrooke-car-test.npz"
+
+## Training
+
+# CrowdAI car
+
+npz="/datasets/BigLearning/ahjiang/bb/npz/crowdai-car-training-0.8.npz"
+labels="/datasets/BigLearning/ahjiang/bb/udacity-od-crowdai/Udacity_object_dataset/labels/car-label.txt"
+outmodel="data/models/exp/yolov2-model-car-yolo2"
